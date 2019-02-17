fbe65878d617e7b751af540a499fbd72d71b717f
selenaqian <selenaq2010@gmail.com>
2019-02-17 02:37:46 -0500

    comparison working

--- screens/LocalTVNewsScreen.js
+++ screens/LocalTVNewsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 // make better styling for this - figure out if can print variable
@@ -26,28 +26,65 @@ lat: long:
 WSOC channel 9
 lat: long:
 6 pm */
-var sphereKnn = require("sphere-knn"),
-    lookup = sphereKnn([ 'WRAL', {lat: 35.787743, long: -78.644257},
-    'ABC11', {lat: 35.052666, long: -78.878357},
-    'WSOC', {lat: 35.227085, long: -80.843124}])
+let sphereKnn = require("sphere-knn"),
+    lookup = sphereKnn([[35.787743, -78.644257],
+    [35.052666, -78.878357],
+    [35.227085, -80.843124]])
 
-var points = lookup(35, -75, 1)
+// find user location
+let usrLat = 35
+let usrLong = -79
+// TODO: assign usrLat and usrLong with actual location
 
-// for scaling would mb want to make some sort of hashmap that contains locations
-// connected to what location it maps to
-// then use math to already compute what's local and then
-// have them change and look up values
+let points
+points = lookup(usrLat, usrLong, 1)
+closestLat = points[0];
+closestLat = closestLat[0];
 
-export default class TVNews extends Component {
+// set first two latitudes - won't need WSOC since that's the else case
+let wralLat = 35.787743
+let abcLat = 35.052666
+
+let message
+let city
+let channelName
+export default class DetermineLocation extends Component {
+  log_function = () => {
+    console.log(points)
+  }
   render() {
-    return (
-      <View>
-        <Text style={styles.header}>Watch</Text>
-        <Text style={[styles.header, styles.underline]}>Channel 5</Text>
-        <Text style={styles.header}>at <Text style={[styles.header,
-          styles.underline]}>6 PM today</Text></Text>
-        <Text style={styles.header}>for local news</Text>
+    if (closestLat === wralLat) {
+      //this.props = {WRAL: true};
+      city = "Raleigh"
+      channelName = "WRAL"
+      message = "Channel 5"
+    }
+    else if (closestLat === abcLat) {
+      //this.props = {ABC11: true};
+      city = "Fayetteville"
+      channelName = "ABC"
+      message = "Channel 11"
+    }
+    else {
+      //this.props = {WSOC: true};
+      city = "Charlotte"
+      channelName = "WSOC"
+      message = "Channel 9"
+    }
+  return (
+      <View style={{flex: 1, flexDirection: 'column'}}>
+      <View style={{flex: 1, flex: 1, backgroundColor: 'white'}}/>
+      <Text style={styles.header}>We have detected that</Text>
+      <Text style={styles.header}>you are from</Text>
+      <Text style={styles.header}>{city}.</Text>
+      <Text style={styles.header}>Watch</Text>
+      <Text style={styles.header}>{channelName} on</Text>
+      <Text style={[styles.header, styles.underline]}>{message}</Text>
+      <Text style={styles.header}>at <Text style={[styles.header,
+        styles.underline]}>6 PM</Text></Text>
+      <Text style={styles.header}>for local news!</Text>
+      <View style={{flex: 1, flex: 1, backgroundColor: 'white'}}/>
       </View>
-    );
+  );
   }
 }
